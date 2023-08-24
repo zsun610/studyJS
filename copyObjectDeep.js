b67131ec008104d198c8cc7f461ad8c0735def4a
Jisun <zsun610@gmail.com>
2023-08-24 22:57:25 +0900

    studied

---
+++ copyObjectDeep.js
@@ -0,0 +1,32 @@
+var copyObjectDeep = function(target){
+  var result = {};
+  if (typeof target === 'object' && target !== null){
+    for (var prop in target){
+      result[prop] = copyObjectDeep(target[prop]);
+    }
+  }
+  else{
+    result = target;
+  }
+  return result;
+}
+
+var obj = {
+  a : 1,
+  b : {
+    c: null,
+    d : [1,2]
+  }
+};
+
+var obj2 = copyObjectDeep(obj)
+
+obj2.a =3;
+obj2.b.c = 4;
+obj.b.d[1] = 3;
+
+console.log(obj);
+console.log(obj2)
+
+// { a: 1, b: { c: null, d: [ 1, 3 ] } }
+// { a: 3, b: { c: 4, d: { '0': 1, '1': 2 } } }
